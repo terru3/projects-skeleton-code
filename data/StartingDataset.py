ec492d1b915bbd10c4e81dd68ba0fdf6899eea41
Terry Ming <96094168+terru3@users.noreply.github.com>
2022-04-21 17:59:58 -0700

    Add files via upload

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -1,19 +1,32 @@
 import torch
-
+from PIL import Image
+import torchvision.transforms as transforms
+import pandas as pd
+import os
 
 class StartingDataset(torch.utils.data.Dataset):
     """
-    Dataset that contains 100000 3x224x224 black images (all zeros).
+    Dataset that contains:
+        csv files with image_id and image_label
+        real image directory
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, image_dir = "train_images/", csv_dir = "."):
+        self.train_csv = pd.read_csv(csv_dir + "/train.csv")
+        self.image_dir = image_dir
 
     def __getitem__(self, index):
-        inputs = torch.zeros([3, 224, 224])
-        label = 0
+        image_id, image_label = self.train_csv.iloc[index]
 
-        return inputs, label
+        # get the images and transform to tensor 
+        image  = Image.open(self.image_dir + image_id)
+        image.resize((224, 224))
+        image_tensor = transforms.ToTensor()(image)
+
+        # get the labels 
+        label = image_label
+
+        return image_tensor, label
 
     def __len__(self):
-        return 10000
+        return len(self.train_csv)
