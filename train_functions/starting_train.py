16800b8f939e5618b862195ba009b790364ef287
Terry Ming <96094168+terru3@users.noreply.github.com>
2022-05-27 19:00:48 -0700

    Add files via upload

--- train_functions/starting_train.py
+++ train_functions/starting_train.py
@@ -6,10 +6,12 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 from torch.utils.tensorboard import SummaryWriter
 
-from data.StartingDataset import StartingDataset
-from networks.StartingNetwork import StartingNetwork
+from StartingDataset import StartingDataset
+from StartingNetwork import StartingNetwork
 
-def starting_train(train_dataset, val_dataset, model, hyperparameters, n_eval, device):
+def starting_train(train_data, val_data,
+                   train_dataset, val_dataset,
+                   model, hyperparameters, n_eval, device):
     """
     Trains and evaluates a model.
 
@@ -22,7 +24,7 @@ def starting_train(train_dataset, val_dataset, model, hyperparameters, n_eval, d
     """
     # Set model to train mode
     model.train()
-    
+
     # Move model to GPU
     model = model.to(device)
 
@@ -31,27 +33,22 @@ def starting_train(train_dataset, val_dataset, model, hyperparameters, n_eval, d
 
     # Initialize dataloaders
     train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=batch_size, shuffle=True
+        train_data, batch_size=batch_size, sampler=train_dataset
     )
     val_loader = torch.utils.data.DataLoader(
-        val_dataset, batch_size=batch_size, shuffle=True
+        val_data, batch_size=batch_size, sampler=val_dataset
     )
 
-    # Initalize optimizer and loss function
-    optimizer = optim.AdamW(model.parameters(), lr=0.001)
+    # Initialize optimizer and loss function
+    optimizer = optim.AdamW(model.parameters())
+
 
     # can add weight_decay=0.01 as parameter for example—L2
+
     # in network: self.dropout = nn.Dropout(p=0.2) for dropout
     # inside forward(): x = self.dropout(x)
 
     loss_fn = nn.CrossEntropyLoss()
-    
-    my_transforms = transforms.Compose([
-        transforms.GaussianBlur(kernel_size = (5, 9), sigma = (0.1, 0.2)),
-        transforms.RandomHorizontalFlip(p=0.5),
-        transforms.RandomVerticalFlip(p=0.5),
-        transforms.RandomRotation(degrees=(0, 180))
-    ])
 
     step = 0
     writer = SummaryWriter()
@@ -67,9 +64,6 @@ def starting_train(train_dataset, val_dataset, model, hyperparameters, n_eval, d
             # Move to GPU
             images = images.to(device)
             labels = labels.to(device)
-            
-            # Apply transforms
-            images = my_transforms(images)
 
             # Forward propagation
             outputs = model(images)
@@ -92,14 +86,13 @@ def starting_train(train_dataset, val_dataset, model, hyperparameters, n_eval, d
                 # Log the results to Tensorboard.
                 # Don't forget to turn off gradient calculations!
                 writer.add_scalar("Validation accuracy", evaluate(val_loader, model, loss_fn, device)[0], epoch + 1)
+
+            writer.flush()
             step += 1
 
         print('Epoch: ', epoch + 1, 'Loss: ', loss.item())  # print loss of the last batch for each epoch
-
         # testing the evaluate function for now
         evaluate(val_loader, model, loss_fn, device, visualize=True)
-        
-        writer.flush()
     writer.close()
 
 def evaluate(val_loader, model, loss_fn, device, visualize=False):
@@ -107,13 +100,12 @@ def evaluate(val_loader, model, loss_fn, device, visualize=False):
     Computes the loss and accuracy of a model on the validation dataset.
 
     """
-        
+
     # Set model to evaluate mode
     model.eval()
-    
+
     # Pass model to GPU
     model = model.to(device)
-   
 
     correct_num = 0
     total_num = 0
@@ -123,14 +115,13 @@ def evaluate(val_loader, model, loss_fn, device, visualize=False):
         # Passing to GPU
         images = images.to(device)
         labels = labels.to(device)
-
         predictions = model(images).argmax(axis=1)
   # output has row number of batch_size, and col number 1 (reduced from 5)
         correct_num += (predictions == labels).sum().item()
         total_num += len(labels)
-        
-    print("Accuracy: ", 100*(correct_num / total_num), "%")
-    
+
+    print("\n Accuracy: ", 100*(correct_num / total_num), "%")
+
     # Visualises sample images with their predicted and actual labels
     classes = ["Cassava Bacterial Blight (CBB)", "Cassava Brown Streak Disease (CBSD)",
                "Cassava Green Mottle (CGM)", "Cassava Mosaic Disease (CMD)",
@@ -139,10 +130,9 @@ def evaluate(val_loader, model, loss_fn, device, visualize=False):
         for i in range(2):
             print("Prediction: ", classes[predictions[i]])
             print("Label: ", classes[labels[i]])
-            plt.imshow(images[i].cpu().permute(1,2,0))
+            plt.imshow(images[i].cpu().permute(1,2,0).astype('uint8'))
             plt.show()
-            
-    performance_summary = [100*(correct_num / total_num)]
-    # to-do: also add validation loss! how to compute?
-    return performance_summary
 
+    performance_summary = [100*(correct_num / total_num)]
+    # to-do: also add validation loss
+    return performance_summary
