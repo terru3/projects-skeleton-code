227b51a31a6f4cbdb6b25990080b403a185a737e
Terry Ming <96094168+terru3@users.noreply.github.com>
2022-05-27 18:57:27 -0700

    Add files via upload

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -4,6 +4,13 @@ import torchvision.transforms as transforms
 import pandas as pd
 import os
 
+my_transforms = transforms.Compose([
+            transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 0.2)),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomVerticalFlip(p=0.5),
+            transforms.RandomRotation(degrees=(0, 180))
+        ])
+
 class StartingDataset(torch.utils.data.Dataset):
     """
     Dataset that contains:
@@ -11,17 +18,30 @@ class StartingDataset(torch.utils.data.Dataset):
         real image directory
     """
 
-    def __init__(self, image_dir = "../train_images/", csv_dir = ".."):
-        self.train_csv = pd.read_csv(csv_dir + "/train.csv")
+    def __init__(self, image_dir = "/Users/Terru/Desktop/UCLA/ACM AI/Projects/projects-skeleton-code-master/train_images/",
+                 csv_dir = "", train=False):
+        self.train_csv = pd.read_csv(csv_dir + "/Users/Terru/Desktop/UCLA/ACM AI/Projects/projects-skeleton-code-master/train.csv")
         self.image_dir = image_dir
+        self.train = train
 
     def __getitem__(self, index):
         image_id, image_label = self.train_csv.iloc[index]
 
         # get the images and transform to tensor
         image  = Image.open(self.image_dir + image_id)
-        image = image.resize((120, 120))
+        image = image.resize((224, 224))
         image_tensor = transforms.ToTensor()(image)
+
+        # normalize
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225])
+        image_tensor = normalize(image_tensor)
+
+        # transformations
+        if (self.train):
+            image_tensor = my_transforms(image_tensor)
+
+        # TO-DO: Only apply transformations if class != 3 (the most common one)
 
         # get the labels
         label = image_label
